4253d4c6a32a98806032fcd8bbae22763d71fcce
jiaminyun <jiaminyun@IT-JIAMINYUN.mango.com>
2017-05-18 09:31:03 +0800

    提交

--- src/main/java/com/my/javabase/Thread.java
+++ src/main/java/com/my/javabase/Thread.java
@@ -1,13 +1,12 @@
 package com.my.javabase;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * Hello world!
- *
  */
-public class Thread 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Thread {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
     }
 }
